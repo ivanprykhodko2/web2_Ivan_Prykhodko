342aa8ac731058e659ee34aa232c777a6c4c7be7
robocodebr <robocodebr@gmail.com>
2020-10-25 20:33:02 +0300

    lessons 8

---
+++ lesson 4/script.js
@@ -0,0 +1,85 @@
+/* Переменные имя и фамилия выводим в консоль 
+let name = 'Ivan';
+let lastname = 'Prykhodko';
+let otchestvo = 'Olegovich';
+let age = 14;
+
+let namef = "Oleg";
+let lastnamef = 'Prykhodko';
+let otchestvof = 'Petrovich';
+let agef ='49';
+
+
+let namem = 'Anna';
+let lastnamem ='Prykhodko';
+let otchestvom = 'Volodimerovna';
+let agem = 36;
+
+    console.log(name);
+    console.log(lastname);
+    console.log(otchestvo);
+    console.log(age)
+
+    console.log(namef);
+    console.log(lastnamef);
+    console.log(otchestvof);
+    console.log(agef)
+
+    console.log(namem);
+    console.log(lastnamem);
+    console.log(otchestvom);
+    console.log(agem)
+    */
+
+/* переменная с рандомным числом 
+let m = (Math.random()*666);
+alert(m)
+
+
+let n = (Math.random()*200)-100;
+alert(n)
+
+
+
+let k = (Math.random()*55)-15;
+alert(k)
+*/
+
+/*округление
+let f = (Math.random()*100)-200;
+
+    console.log(Math.round(f));
+    console.log(Math.floor(f));
+    console.log(Math.ceil(f));
+*/
+
+/*степень
+console.log(Math.pow(8,2));
+console.log(Math.pow(3,2));
+console.log(Math.pow(16,2));
+*/
+
+/* корень 
+console.log(Math.sqrt(16));
+*/
+
+/*Повторение
+console.log(Math.pow(7,5));
+console.log(Math.sqrt(10));
+*/
+/*
+console.log(Math.min(Math.random()*10, Math.random()*10, Math.random()*10, Math.random()*10))
+console.log(Math.max(Math.random()*10, Math.random()*10, Math.random()*10, Math.random()*10))
+*/
+let a = Math.floor(Math.random()*25);
+let b = Math.floor(Math.random()*20);
+let s = Math.floor(Math.random()*10);
+let d = Math.floor(Math.random()*15);
+let e = Math.floor(Math.random()*35);
+    console.log(a);
+    console.log(b);
+    console.log(s);
+    console.log(d);
+    console.log(e);
+        console.log(Math.min(a, b, s, d, e));
+        console.log(Math.max(a, b, s, d, e));
---
+++ lesson 6/script.js
@@ -0,0 +1,79 @@
+/*
+let bool = confirm("ti ")
+*/
+/*
+let age = prompt("Skolko tebe let :", " ")
+alert ("tebe " + age + " let ")
+
+let h = prompt(" tvoyo hobbi :", " ")
+alert ("tvoyo hobbi " + h)
+let isA = confirm("ti soglasen?")
+alert(isA); 
+*/
+/*
+let a = prompt("storona kvadrata ")
+console.log(Math.pow(a,2))
+*/
+/*
+let a = parseInt(prompt("skoko tebe let "))
+
+if (a >= 18){
+    alert("zahodi");
+}
+*/
+/*
+if (bool){
+    alert("tvoy pk vzloman ");
+    
+}
+*/
+/*
+let a = Math.random()*50;
+if ( a >= 0 && a <= 25 || a >= 50 && a <= 100) {
+    alert("a > 50");
+}
+*/
+/*
+if (r <= 20){
+    alert
+}
+*/
+/*
+let a = 5
+if(a==5){
+    alert("a = 5");
+
+}
+if (a != 4){
+    alert(" a!=5")
+}
+*/
+/*
+console.log( 5 > 6 );
+console.log (10 == 10);
+console.log(-10 < -20);
+console.log(20<=20);
+console.log(47!=54);
+console.log(10>=0);
+console.log(10==11);
+console.log(-10<3);
+console.log(0>=0);
+
+let ran = Math.round(Math.random()*2);
+if(ran == 1){
+    console.log("ran=1");
+}else if (ran==2){
+    console.log("ran=2");
+}else{
+    console.log("ran=0");
+}
+console.log(ran);
+*/
+let a = 5 % 4;
+let b = 125 % 100;
+let c = 285.5 % 140.0
+let d = 30.0 % 33.0
+console.log(a);
+console.log(b);
+console.log(c);
+console.log(d);
